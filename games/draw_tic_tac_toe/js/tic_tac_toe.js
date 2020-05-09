9f087f5c908853264b61f89b380e6c917892c5ed
vynny21 <vynnyprudencio@gmail.com>
2020-05-08 23:53:14 -0300

    game tic-tac-toe

---
+++ games/draw_tic_tac_toe/js/tic_tac_toe.js
@@ -0,0 +1,92 @@
+// TIC TAC TOE
+const tic_tac_toe = {
+
+  // ATTRIBUTES
+  board: ['','','','','','','','',''],
+  symbols: {
+              options: ['O','X'],
+              turn_index: 0,
+              change: function(){
+                  this.turn_index = ( this.turn_index === 0 ? 1:0 );
+              }
+          },
+  container_element: null,
+  gameover: false,
+  winning_sequences: [
+                      [0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [2,4,6]
+                  ],
+
+  // FUNCTIONS
+  
+  make_play: function(position) {
+      if (this.gameover) return false;
+      if (this.board[position] === ''){
+          this.board[position] = this.symbols.options[this.symbols.turn_index];
+          let winning_sequences_index = this.check_winning_sequences( this.symbols.options[this.symbols.turn_index] );
+          if (winning_sequences_index >= 0){
+              this.setWinnerSequenceIndex(winning_sequences_index);
+              this.game_is_over();
+
+              setTimeout(() => {
+                  this.start()
+              }, 2000);
+          } else{
+              this.symbols.change();
+          }
+          return true;
+      }
+      else {
+          return false;
+      }
+  },
+
+  check_winning_sequences: function(simbol) {
+
+      for ( i in this.winning_sequences ) {
+          if (this.board[ this.winning_sequences[i][0] ] == simbol  &&
+              this.board[ this.winning_sequences[i][1] ] == simbol &&
+              this.board[ this.winning_sequences[i][2] ] == simbol) {
+              console.log('winning sequences INDEX:' + i);
+              return i;
+          }
+      };
+      return -1;
+  },
+
+  game_is_over: function() {
+      this.gameover = true;
+      console.log('GAME OVER');
+  },
+
+  start: function() {
+      this.board.fill('');
+      this.gameover = false;       
+      this.setWinnerSequenceIndex(null);
+  },
+
+  //INTERFACES TO RENDER-CORE
+  winner_sequence: null,
+  isGameover: function(){
+      return this.gameover
+  },
+  getBoardPosition: function(position){
+      return this.board[position];
+  },
+  setWinnerSequenceIndex(position){
+      this.winner_sequence = position;
+  },
+  getWinnerSequence: function(){
+      return this.winning_sequences[this.winner_sequence];
+  },
+  executeMakePlay: function(position){
+      this.make_play(position);
+  }
+
+};
